b8c94cd2d1cc3b8971471d65621930197aebf098
luisriverovillalba8-sys <luisriverovillalba8@gmail.com>
2026-09-15 20:37:35 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('form-inscripcion').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('¡Formulario enviado! Pronto lo conectaremos a la base de datos de Supabase.');
+});
